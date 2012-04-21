7931b955843837f8c1fe621a6c09efeba5ff4e99
Fred Chien <cfsghost@gmail.com>
2012-04-21 08:10:46 +0800

    add app.js and package.json

---
+++ app.js
@@ -0,0 +1,7 @@
+var DBHouse = require('dbhouse');
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Simple Server\n');
+}).listen(80);
