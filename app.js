bea6486120a7693f8a104317e0e0aaaa2468a3f9
Fred Chien <fred@mandice.com>
2012-08-03 13:53:27 +0800

    remove unneccessary filex

--- app.js
+++
@@ -1,7 +0,0 @@
-var DBHouse = require('dbhouse');
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Simple Server\n');
-}).listen(80);
